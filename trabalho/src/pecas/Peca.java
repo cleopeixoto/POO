9220383094bfcffd0eb2aeed4c5ff26ed13d2bef
cleopeixoto <cleoxinha@gmail.com>
2018-05-27 20:59:15 -0300

    consertando erros

--- trabalho/src/pecas/Peca.java
+++ trabalho/src/pecas/Peca.java
@@ -5,15 +5,25 @@ import java.awt.Image;
 public class Peca {
 	
 	private tipoDaPeca tipo;
-	private int posX, posY;
-	private int cor; // 0 brancas 1 pretas
+	protected int posX, posY;
+	protected int cor; // 0 brancas 1 pretas
 	private Image imgPeca;
 	
-	public void movimento (int xi, int yi, int xf, int yf) {
-		
+	public Peca (int x, int y, int cor) {
+		posX = x;
+		posY = y;
+		this.cor = cor;
 	}
 	
-	public void setCor (int x) {
+	public void movimento (Peca pecas[][], int x, int y) throws MoveException {
+		if (x < 0 || x > 8 || y < 0 || y > 8)
+			throw new MoveException ("Fora dos limites");
+		
+		if (posX == x && posY == y)
+			throw new MoveException ("Movimento invalido");
+	}
+	
+	public void setCor (int x) { 
 		this.cor = x;
 	}
 	
@@ -24,6 +34,14 @@ public class Peca {
 	public void setPos (int x, int y) {
 		this.posX = x;
 		this.posY = y;
+	}
+	
+	public int getX () {
+		return posX;
+	}
+	
+	public int getY () {
+		return posY;
 	}
 	
 	public void setImg(Image imgPeca) {
@@ -40,5 +58,13 @@ public class Peca {
 	
 	public void setTipo (tipoDaPeca tipo) {
 		this.tipo = tipo;
+	}
+	
+	public int getALT () {
+		return imgPeca.getWidth(null);
+	}
+	
+	public int getLARG () {
+		return imgPeca.getHeight(null);
 	}
 }
